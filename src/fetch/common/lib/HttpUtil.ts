3c068c2452057cb4540d5c9ccc361281de60a1d6
lucas99freelas <lucas99freelas@99freelas.com>
2020-09-16 08:53:18 -0300

    [dev] fetch

--- src/fetch/common/lib/HttpUtil.ts
+++ src/fetch/common/lib/HttpUtil.ts
@@ -25,50 +25,6 @@ const rawPut = <T>(url: string, data?: T, params?: HttpRequestParams) =>
 const rawPatch = <T>(url: string, data?: T, params?: HttpRequestParams) =>
 	axios.patch(url, { data, ...params });
 
-// type Arr = readonly unknown[];
-
-// const wrap = <T extends Arr, K>(
-// 	fn: (...args: [...T]) => Promise<AxiosResponse<K>>
-// ) => (...args: [...T]) =>
-// 	fn(...args)
-// 		.catch(function (error) {
-// 			if (error.response) {
-// 				// The request was made and the server responded with a status code
-// 				// that falls out of the range of 2xx
-// 				console.log(error.response.data);
-// 				console.log(error.response.status);
-// 				console.log(error.response.headers);
-// 			} else if (error.request) {
-// 				// The request was made but no response was received
-// 				// `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-// 				// http.ClientRequest in node.js
-// 				console.log(error.request);
-// 			} else {
-// 				// Something happened in setting up the request that triggered an Error
-// 				console.log('Error', error.message);
-// 			}
-// 			console.log(error.config);
-
-// 			return error.response;
-// 		})
-// 		.then((response) => {
-// 			if (!response) {
-// 				return null;
-// 			}
-
-// 			const { status, data } = response;
-// 			const resp: HttpResponse<K> = { status, data };
-// 			return resp;
-// 		});
-
-// const del = wrap(rawDel);
-// const get = wrap(rawGet);
-// const head = wrap(rawHead);
-// const options = wrap(rawOptions);
-// const patch = wrap(rawPatch);
-// const post = wrap(rawPost);
-// const put = wrap(rawPut);
-
 const wrap = <T extends Array<unknown>, K>(
 	fn: (...args: T) => Promise<AxiosResponse<K>>
 ) => (...args: T) =>
